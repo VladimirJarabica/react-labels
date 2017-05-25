8dc053e9eb597fd594585efb12cfb612fc937af8
VladimirJarabica <vladojarabica@gmail.com>
2017-05-25 19:41:57 +0200

    init babel and flow

---
+++ src/Labels.js
@@ -0,0 +1,13 @@
+// @flow
+import React, { Component } from "react"
+
+export default class Labels extends Component {
+  props: {
+    labels: Array<string|Object>,
+  }
+  render() {
+    return (
+      <div></div>
+    )
+  }
+}
